f7c903c831d8a373206f994934ccff1cdc8f22e2
olegmyltsev <oleg.myltsev.2008@gmail.com>
2023-01-31 14:58:54 +0300

    sound

--- src/js/sound.js
+++ src/js/sound.js
@@ -1,12 +1,12 @@
 const getBtn = document.getElementsByClassName("btn")
-function buttonSound(treck, timeout) {
-    let audio = new Audio("../audio/" + treck + ".mp3")
+function buttonSound() {
+    let audio = new Audio("audio/button.mp3")
     audio.play()}
 for (let buttonsCount = 0; buttonsCount < getBtn.length; buttonsCount++) {
-    getBtn[buttonsCount].addEventListener("click",function (){ buttonSound("button") })
+    getBtn[buttonsCount].addEventListener("click",function (){ buttonSound() })
 }
 function rightSound() {
-    let rightAudio = new Audio("../audio/righte.mp3")
+    let rightAudio = new Audio("audio/righte.mp3")
     rightAudio.play()
 }
 
